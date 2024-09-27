59b306da0b3a2b8f2c06479f07a713cdda647734
ranialouhibi <146093069+ranialouhibi@users.noreply.github.com>
2024-09-27 10:41:21 +0200

    skrev hei

--- scripts/your-army.js
+++ scripts/your-army.js
@@ -2,6 +2,7 @@ import warriorModule from "./modules/warriorModule.js";
 
 const warriors =  warriorModule().getWarriors();
 console.log(warriors);
+// hei
  
 // The code above will log the warriors array to the console.
 // The code below will log the warriors array to the console.
