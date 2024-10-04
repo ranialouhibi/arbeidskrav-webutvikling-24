540c5f8245f757d3dc83729bd1b96e0dcd52d634
cathrinaquach <cathrinathanhq@gmail.com>
2024-10-04 21:08:14 +0200

    hei

--- scripts/your-army.js
+++ scripts/your-army.js
@@ -5,11 +5,6 @@ const warriors =  warriorModule().getWarriors();
 console.log(warriors);
 
 
-// The code above will log the warriors array to the console.
-// The code below will log the warriors array to the console.
-
-
-
 const outputSection = document.querySelector("#output-section");
 
 const showWarriors = () => {
