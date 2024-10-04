f89c7bca7511dac2976542e5d5d3fd3da773372c
cathrinaquach <cathrinathanhq@gmail.com>
2024-10-04 17:33:27 +0200

    hei

--- scripts/your-army.js
+++ scripts/your-army.js
@@ -12,5 +12,22 @@ console.log(warriors);
 const outputSection = document.querySelector("#output-section");
 
 const showWarriors = () => {
-  if(localStorage.getItem(""))
+  if(localStorage.getItem("purchasedWarriors") != null);{
+
+  const purchasedWarriors = localStorage.getItem("purchasedWarriors");
+  console.log(localStorage);
+
+  console.log(localStorage.getItem("purchasedWarriors"));
+
+  outputSection.innerHTML = `
+   <article>
+   <h3>${purchasedWarriors}</h3>
+   <img src="images/${purchasedWarriors}.jpg>
+   </article>
+  `;
+  }
 }
+
+(() =>{
+  showWarriors();
+})();
