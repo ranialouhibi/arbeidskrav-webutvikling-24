6a31b32a8169754fc7fea206e10ab0533f6e3beb
cathrinaquach <cathrinathanhq@gmail.com>
2024-10-04 19:36:05 +0200

    hei

--- scripts/your-army.js
+++ scripts/your-army.js
@@ -1,13 +1,14 @@
-/*
+
 import warriorModule from "./modules/warriorModule.js";
 
 const warriors =  warriorModule().getWarriors();
 console.log(warriors);
-*/
 
- 
+
 // The code above will log the warriors array to the console.
 // The code below will log the warriors array to the console.
+
+
 
 const outputSection = document.querySelector("#output-section");
 
